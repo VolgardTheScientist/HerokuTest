00e0a757c44c696c9d6d9c6f781ea208bb5faeb4
VolgardTheScientist <piotrpawelpiotrowski@gmail.com>
2023-09-01 15:32:08 +0200

    Setup.py added for Heroku

---
+++ vendor/ifcpatch/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ifcpatch',
+    version='0.1',
+    packages=find_packages(),
+    author='Dion Moult',
+    author_email='dion@thinkmoult.com',
+    url='https://github.com/IfcOpenShell/IfcOpenShell/tree/v0.7.0/src/ifcpatch',
+    license='GNU Lesser General Public License'
+)
